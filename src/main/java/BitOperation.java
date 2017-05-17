9a03e1bb41cbf968732e9b70596dc1e3be6ccebb
liliang <l_liang@apple.com>
2017-05-16 20:47:08 -0700

    BigInteger to binary

--- src/main/java/BitOperation.java
+++ src/main/java/BitOperation.java
@@ -1,3 +1,7 @@
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.security.InvalidParameterException;
+
 /**
  * Created by lliang on 5/13/2017.
  */
@@ -6,7 +10,7 @@ public class BitOperation {
     public static int updateBits(int n, int m, int i, int j) {
         int max = ~0; /* All 1’s */
         // 1’s through position j, then 0’s
-        int left = max - ((1 << j+1) - 1);
+        int left =  max << j+1;
 
         System.out.println(Integer.toBinaryString(left));
 
@@ -26,5 +30,60 @@ public class BitOperation {
         return (n & mask) | (m << i);
     }
 
+    /**
+     * convert a decimal into binary string
+     * @param num 0<num<1
+     * @return num in binary format
+     */
+    public static String convertDecimalPartToBinary(double num) {
+        if (num >= 1 || num <= 0) {
+            throw new InvalidParameterException("The parameter should be between 0 and 1.");
+        }
 
+        StringBuilder binary = new StringBuilder();
+        binary.append(".");
+        while (num > 0) {
+			/* Setting a limit on length: 32 characters */
+            if (binary.length() > 32) {
+                throw new InvalidParameterException("The length of the digits are longer than 32.");
+            }
+            double r = num * 2;
+            if (r >= 1) {
+                binary.append(1);
+                num = r - 1;
+            } else {
+                binary.append(0);
+                num = r;
+            }
+        }
+        return binary.toString();
+    }
+
+    public static String convertNonDecimalToBinary(final BigInteger nonDecimalPart){
+        StringBuilder binary = new StringBuilder();
+        BigInteger quotient = nonDecimalPart;
+        BigInteger remainder;
+        final BigInteger BIG_TWO = new BigInteger("2");
+        final BigInteger BIG_ONE = new BigInteger("1");
+
+        while (quotient.compareTo(BIG_TWO) >= 0 ){
+            remainder = quotient.remainder(BIG_TWO);
+            quotient = quotient.divide(BIG_TWO);
+            binary.append(remainder.toString());
+        }
+        binary.append(quotient.toString());
+
+        return binary.reverse().toString();
+    }
+
+    public static String convertDecimalToBinary(String decimalStr){
+        int  dotIndex = decimalStr.indexOf(".");
+        String decimalPart = decimalStr.substring(dotIndex);
+        String nonDecimalPart = decimalPart.substring(0, dotIndex);
+
+        BigDecimal afterDotVal = new BigDecimal(decimalPart);
+        BigDecimal nonDecimalVal = new BigDecimal(nonDecimalPart);
+
+        return "";
+    }
 }
--- src/test/java/BitOperationTest.java
+++ src/test/java/BitOperationTest.java
@@ -1,19 +1,50 @@
 import junit.framework.TestCase;
+import org.junit.Assert;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
 
 public class BitOperationTest extends TestCase {
 
     public void testUpdateBits() throws Exception {
+
         int n = 0b10000000000;
         int m = 0b10101;
 
-        //int result = BitOperation.updateBits(n, m, 2, 6);
-        //System.out.println(Integer.toString(result,2));
+        int result = BitOperation.updateBits(n, m, 2, 6);
+        System.out.println(Integer.toString(result,2));
+
+        Assert.assertTrue(result == 0b10001010100);
 
         n = 0b11111111111;
         m = 0b00101;
 
-        int result = BitOperation.updateBits(n, m, 2, 6);
+        result = BitOperation.updateBits(n, m, 2, 6);
         System.out.println(Integer.toString(result,2));
+
+        Assert.assertTrue(result == 0b11110010111);
+    }
+
+    public void testConvertNonDecimalToBinary(){
+        String bigStr = "1";
+        BigInteger num = new BigInteger(bigStr);
+        String numStr = BitOperation.convertNonDecimalToBinary(num);
+        Assert.assertTrue(numStr.equals(bigStr));
+
+        bigStr = "2";
+        num = new BigInteger(bigStr);
+        numStr = BitOperation.convertNonDecimalToBinary(num);
+        Assert.assertTrue(numStr.equals(Integer.toBinaryString(Integer.valueOf(bigStr))));
+
+        bigStr = "20";
+        num = new BigInteger(bigStr);
+        numStr = BitOperation.convertNonDecimalToBinary(num);
+        Assert.assertTrue(numStr.equals(Integer.toBinaryString(Integer.valueOf(bigStr))));
+
+        bigStr = "2009";
+        num = new BigInteger(bigStr);
+        numStr = BitOperation.convertNonDecimalToBinary(num);
+        Assert.assertTrue(numStr.equals(Integer.toBinaryString(Integer.valueOf(bigStr))));
 
     }
 }
