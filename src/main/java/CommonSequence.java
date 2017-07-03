b636225781faf7a8d2fdee835b50995880f3a202
Li Liang <coderlliang@gmail.com>
2017-07-02 22:31:16 -0700

    longest common sequence

---
+++ src/main/java/CommonSequence.java
@@ -0,0 +1,126 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by lliang on 7/2/2017.
+ * A sub-sequence of a given sequence is just the given sequence with zero or
+ more elements left out. Formally, given a sequence X = (x1; x2; : : : ; xm), another
+ sequence Z D= (z1; z2; : : :; zk) is a sub-sequence of X if there exists a strictly
+ increasing sequence (i1; i2; : : : ; ik) of indices of X such that for all j = 1; 2; : : : ; k,
+ we have Xij = Zj. For example, Z = (B; C; D; B) is a sub-sequence of X D
+ (A;B;C;B;D; A;B) with corresponding index sequence (2; 3; 5; 7).
+ */
+public class CommonSequence {
+    public static final int LETTER_COUNT = 26;
+
+    public static class MatchInfo{
+        char ch;
+        int position;
+        int posOnOtherArray;
+
+        public MatchInfo(char ch, int position, int posOnOtherArray) {
+            this.ch = ch;
+            this.position = position;
+            this.posOnOtherArray = posOnOtherArray;
+        }
+
+
+    }
+    public static List<List<List<MatchInfo>>> findLongest(char[] charsA, char[] charsB){
+        List<List<List<MatchInfo>>> common = new ArrayList<>();
+        char[] longerChars, shorterChars;
+        if (charsA.length > charsB.length){
+            longerChars = charsA;
+            shorterChars = charsB;
+        }else {
+            longerChars = charsB;
+            shorterChars = charsA;
+        }
+        List<List<Integer>> signature = buildSignature(longerChars);
+        addOneMoreLetter(shorterChars, common, 1, signature);
+        return common;
+    }
+
+    public static void addOneMoreLetter(char[] shorterArray,  List<List<List<MatchInfo>>> common,
+                                        int level,  List<List<Integer>> signature){
+        if (level > shorterArray.length){
+            return;
+        }
+        if (1 == level){
+            List<List<MatchInfo>> lists = new ArrayList<>();
+            for (int i = 0; i < shorterArray.length; i++){
+                int posOnOtherArray = getLetterPosition(signature, shorterArray[i], 0);
+                if (-1 < posOnOtherArray){
+                    MatchInfo matchInfo = new MatchInfo(shorterArray[i], i, posOnOtherArray);
+                    List<MatchInfo> matchInfoList = new ArrayList<>();
+                    matchInfoList.add(matchInfo);
+                    lists.add(matchInfoList);
+                }
+            }
+            common.add(level-1,lists);
+        }else {
+            List<List<MatchInfo>> longerSequences = common.get(level-2);
+            List<List<MatchInfo>> lists = new ArrayList<>();
+
+            for (List<MatchInfo> matchInfoList : longerSequences){
+                MatchInfo matchInfo = matchInfoList.get(matchInfoList.size()-1);
+                for (int i = matchInfo.position+1; i < shorterArray.length; i++) {
+                    int posOnOtherArray = getLetterPosition(signature, shorterArray[i], matchInfo.posOnOtherArray + 1);
+                    if (-1 < posOnOtherArray) {
+                        MatchInfo newMatchInfo = new MatchInfo( shorterArray[i], i, posOnOtherArray);
+                        List<MatchInfo> newList = cloneList(matchInfoList);
+                        newList.add(newMatchInfo);
+                        lists.add(newList);
+                    }
+                }
+            }
+            common.add(level-1,lists);
+        }
+        addOneMoreLetter(shorterArray, common, level+1,signature);
+    }
+    public static boolean isInSequence(List<Character> common, char toFind){
+        boolean ret = false;
+        for (Character ch : common){
+            if (ch == toFind){
+                ret = true;
+                break;
+            }
+        }
+        return ret;
+    }
+
+    public static int getLetterPosition( List<List<Integer>> signature, char ch, int cursor){
+
+        int ret = -1;
+        int index = Character.toLowerCase(ch) - 'a';
+        List<Integer> letterPositions = signature.get(index);
+        for (Integer position : letterPositions){
+            if (position >= cursor){
+                ret = position;
+                break;
+            }
+        }
+        return  ret;
+    }
+
+    public static List<List<Integer>> buildSignature(char[] chars){
+        List<List<Integer>> ret =new ArrayList<>(LETTER_COUNT);
+        for (int i = 0; i <LETTER_COUNT; i++ ){
+            List<Integer> integerList = new ArrayList<>();
+            ret.add(integerList);
+        }
+        for(int i = 0; i < chars.length; i++){
+            int index = Character.toLowerCase(chars[i]) - 'a';
+            List<Integer> list = ret.get(index);
+            list.add(i);
+        }
+        return ret;
+    }
+    public static List<MatchInfo> cloneList(List<MatchInfo> list) {
+        List<MatchInfo> clone = new ArrayList<>(list.size());
+        for (MatchInfo item : list){
+            clone.add(new MatchInfo(item.ch, item.position, item.posOnOtherArray));
+        }
+        return clone;
+    }
+}
---
+++ src/test/java/CommonSequenceTest.java
@@ -0,0 +1,54 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class CommonSequenceTest {
+
+    @Test
+    public void testFindLongest() throws Exception {
+        char[] charsA = {'a', 'b'};
+        char[] charsB = {'a', 'b'};
+        List<List<List<CommonSequence.MatchInfo>>> lists = CommonSequence.findLongest(charsA, charsB);
+        List<List<CommonSequence.MatchInfo>> longestList = lists.get(lists.size()-1);
+        Assert.assertTrue(1 == longestList.size());
+        List<CommonSequence.MatchInfo> longestSequence = longestList.get(longestList.size() - 1);
+        Assert.assertTrue( 2 == longestSequence.size());
+
+        //three same elements
+        char[] charsA2 = {'a', 'b', 'c'};
+        char[] charsB2 = {'a', 'b', 'c'};
+        lists = CommonSequence.findLongest(charsA2, charsB2);
+        longestList = lists.get(lists.size()-1);
+        Assert.assertTrue(1 == longestList.size());
+        longestSequence = longestList.get(longestList.size() - 1);
+        Assert.assertTrue( 3 == longestSequence.size());
+
+        //four same elements
+        char[] charsA3 = {'a', 'b', 'c', 'd'};
+        char[] charsB3 = {'a', 'b', 'c', 'd'};
+        lists = CommonSequence.findLongest(charsA3, charsB3);
+        longestList = lists.get(lists.size()-1);
+        Assert.assertTrue(1 == longestList.size());
+        longestSequence = longestList.get(longestList.size() - 1);
+        Assert.assertTrue( 4 == longestSequence.size());
+
+        //three different elements
+        char[] charsA4 = {'a', 'b', 'c'};
+        char[] charsB4 = {'e', 'f', 'g'};
+        lists = CommonSequence.findLongest(charsA4, charsB4);
+        longestList = lists.get(lists.size()-1);
+        Assert.assertTrue(0 == longestList.size());
+
+        //from book
+        char[] charsA5 = {'a', 'b', 'c', 'b', 'd', 'a', 'b'};
+        char[] charsB5 = {'b', 'd', 'c', 'a', 'b', 'a'};
+        lists = CommonSequence.findLongest(charsA5, charsB5);
+        longestList = lists.get(lists.size()-3);
+        Assert.assertTrue(3 == longestList.size());
+        longestSequence = longestList.get(longestList.size() - 1);
+        Assert.assertTrue( 4 == longestSequence.size());
+    }
+}
